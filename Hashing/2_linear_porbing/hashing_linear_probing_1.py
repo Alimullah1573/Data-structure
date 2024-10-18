c8d24a9f6fabed46ee2e44b21ba347b3fad3cab5
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2024-10-18 10:40:28 +0600

    Add files via upload

---
+++ Hashing/2_linear_porbing/hashing_linear_probing_1.py
@@ -0,0 +1,45 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
+
+    def hash_function(self, key):
+        return key % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+
+        # Linear probing in case of collision
+        while self.table[index] is not None:
+            index = (index + 1) % self.size
+
+        self.table[index] = (key, value)
+
+    def search(self, key):
+        index = self.hash_function(key)
+
+        while self.table[index] is not None:
+            if self.table[index][0] == key:
+                return self.table[index][1]
+            index = (index + 1) % self.size
+
+        return None
+
+
+# Example usage
+hash_table = HashTable(10)
+hash_table.insert(12, 'Data 1')
+hash_table.insert(22, 'Data 2')
+hash_table.insert(32, 'Data 3')
+hash_table.insert(10,"data 4")
+hash_table.insert(20,"data 5")
+hash_table.insert(30,"data 6")
+
+# Output: 'Data 2'
+print(hash_table.search(22))
+
+# Output data 6
+print(hash_table.search(30))
+
+
+print(hash_table.table)
