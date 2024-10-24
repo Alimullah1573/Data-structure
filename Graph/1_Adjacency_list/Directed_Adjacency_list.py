9d8dd71e7229ee922c38fc97f60bf8e753550df7
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2024-10-24 15:30:06 +0600

    Add files via upload

---
+++ Graph/1_Adjacency_list/Directed_Adjacency_list.py
@@ -0,0 +1,26 @@
+class Graph:
+    def __init__(self, num_of_nodes):
+        self.num_of_nodes = num_of_nodes
+        self.nodes = list(range(num_of_nodes))  # List of nodes
+        self.adj_list = [[] for _ in range(num_of_nodes)]  # Adjacency list initialized with empty lists
+
+    # Add an edge to the graph
+    def add_edge(self, node, neighbor):                     
+        self.adj_list[node].append(neighbor)
+
+
+    # Print the adjacency list
+    def print_graph(self):
+        for node in self.nodes:
+            print(f"{node} -> {self.adj_list[node]}")
+
+# Create a graph with 4 nodes
+g = Graph(4)
+g.add_edge(0, 1)   # 0------>1
+g.add_edge(0, 2)    # 0-------->2
+g.add_edge(1, 2)   # 1--------->2
+g.add_edge(2, 3)   # 2--------->3
+g.add_edge(3, 3)  #  3-------->3
+
+# Print the adjacency list
+g.print_graph()
